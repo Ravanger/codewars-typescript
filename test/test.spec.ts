a685d975da271ec2a81dfd343bbc5f2f18f63c37
AldenDimensionalMedia <Boris.Rozzy@gmail.com>
2021-12-02 20:53:10 -0500

    Weight for weight

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,17 +1,17 @@
 import { assert } from "chai"
-// import { orderWeight } from "../src/Weight for weight"
+import { orderWeight } from "../src/Weight for weight"
 
-// describe("Fixed Tests", function () {
-//   it("orderWeight", function () {
-//     assert.strictEqual(
-//       orderWeight("103 123 4444 99 2000"),
-//       "2000 103 123 4444 99"
-//     )
-//   })
-//   it("orderWeight", function () {
-//     assert.strictEqual(
-//       orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"),
-//       "11 11 2000 10003 22 123 1234000 44444444 9999"
-//     )
-//   })
-// })
+describe("Fixed Tests", function () {
+  it("orderWeight", function () {
+    assert.strictEqual(
+      orderWeight("103 123 4444 99 2000"),
+      "2000 103 123 4444 99"
+    )
+  })
+  it("orderWeight", function () {
+    assert.strictEqual(
+      orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"),
+      "11 11 2000 10003 22 123 1234000 44444444 9999"
+    )
+  })
+})
